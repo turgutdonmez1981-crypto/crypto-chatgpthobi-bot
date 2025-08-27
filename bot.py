ce1c5efe883e9e85ba86794a7c4bfb628f4d14a3
turgutdonmez1981-crypto <turgut.donmez1981@gmail.com>
2025-08-27 17:23:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,1 @@
-ilk önce import os
-from telegram import Bot
-
-TOKEN = os.environ['TELEGRAM_TOKEN']
-CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
-
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="💬 Test mesajı: Chatgpthobi_bot çalışıyor ✅")
+print("Bot çalışıyor ✅")
